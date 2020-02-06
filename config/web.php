6c7f00524dbd2109ecb4eee1561275299dc5b2ee
pavels1982@mail.ru <pavels1982@mail.ru>
2020-02-06 10:59:09 +0300

    Refactoring

--- config/web.php
+++ config/web.php
@@ -5,6 +5,7 @@ $db = require __DIR__ . '/db.php';
 
 $config = [
     'id' => 'basic',
+    'name' => 'RemotionService',
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
     'aliases' => [
@@ -48,6 +49,7 @@ $config = [
 
     ],
     'params' => $params,
+    'defaultRoute' => 'site/about'
 ];
 
 if (YII_ENV_DEV) {
